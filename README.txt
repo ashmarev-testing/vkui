caffc39d887b2e1ef739156d1a2ffc51704794bd
CodeSandbox <support@codesandbox.io>
2023-12-21 19:42:12 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,1 @@
+https://github.com/VKCOM/VKUI
---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+
+export const App = () => {
+  return (
+    <AppRoot>
+      <SplitLayout header={<PanelHeader separator={false} />}>
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <App />
+    </AdaptivityProvider>
+  </ConfigProvider>,
+);
